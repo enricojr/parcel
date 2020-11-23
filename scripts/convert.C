f815df04c889d05aacd0de35e3bdc2c95a2be6e8
enricojr <sc_enricojr@yahoo.it>
2020-11-23 08:25:28 +0100

    effective atomic number

--- scripts/convert.C
+++ scripts/convert.C
@@ -52,6 +52,56 @@ string getElement(const unsigned int Z){
 /*************************************************************************************/
 /*************************************************************************************/
 
+double getMass(const unsigned int Z){
+  if(Z==1) return 1.008;
+  if(Z==2) return 4.0026;
+  if(Z==3) return 6.94;
+  if(Z==4) return 9.0122;
+  if(Z==5) return 10.81;
+  if(Z==6) return 12.011;
+  if(Z==7) return 14.007;
+  if(Z==8) return 15.999;
+  if(Z==9) return 18.998;
+  if(Z==10) return 20.180;
+  if(Z==11) return 22.990;
+  if(Z==12) return 24.305;
+  if(Z==13) return 26.982;
+  if(Z==14) return 28.085;
+  if(Z==15) return 30.974;
+  if(Z==16) return 32.06;
+  if(Z==17) return 35.45;
+  if(Z==18) return 39.948;
+  if(Z==19) return 39.098;
+  if(Z==20) return 40.078;
+  if(Z==21) return 44.956;
+  if(Z==22) return 47.867;
+  if(Z==23) return 50.942;
+  if(Z==24) return 51.996;
+  if(Z==25) return 54.938;
+  if(Z==26) return 55.845;
+  if(Z==27) return 58.933;
+  if(Z==28) return 58.693;
+  if(Z==33) return 74.922;
+  if(Z==35) return 79.904;
+  if(Z==42) return 95.95;
+  if(Z==50) return 118.71;
+  if(Z==53) return 126.90;
+  if(Z==56) return 137.33;
+  if(Z==73) return 180.95;
+  if(Z==82) return 207.2;
+  if(Z==83) return 208.98;
+  if(Z==1010) return 10.;
+  if(Z==1011) return 11.;
+  else{
+    cout << "WARNING!!! ADD NEW ELEMENT TO THE DATABASE, WITH Z = " << Z << endl;
+    return 0.;
+  }
+}
+
+/*************************************************************************************/
+/*************************************************************************************/
+/*************************************************************************************/
+
 void uncapitalize(char *name){
 
   for(unsigned int i=0; i<1000; i++){
@@ -65,7 +115,7 @@ void uncapitalize(char *name){
 /*************************************************************************************/
 /*************************************************************************************/
 
-int parseMaterials(ofstream &fileOut, ofstream &fileOut2, ofstream &fileOut3, ofstream &fileOut4,
+int parseMaterials(ofstream &fileOut, ofstream &fileOut2, ofstream &fileOut3, ofstream &fileOut4, ofstream &fileOut5,
 		   const pair<string, string> fileName){
 
   fileOut << "/////////////////////////////////////////////////////////" << endl;
@@ -92,13 +142,23 @@ int parseMaterials(ofstream &fileOut, ofstream &fileOut2, ofstream &fileOut3, of
     uncapitalize(name);
     fileOut << endl << "// Create " << name << endl;
     fileOut << "G4Material* " << name << " = new G4Material(\"" << name << "\", " << density << " * CLHEP::g / CLHEP::cm3, " << nElements << ");" << endl;
+    double effectiveZ = 0.;
+    double nElectrons = 0.;
     for(unsigned int iElement=0; iElement<nElements; iElement++){
       unsigned int Z;
       double nAtoms;
       getline(fileIn, line);      
       sscanf(line.c_str(), "%d\t%lf", &Z, &nAtoms);
       fileOut << name << "->AddElement(" << getElement(Z) << ", " << nAtoms << ");" << endl;
+      if(nAtoms < 1.) nAtoms /= getMass(Z);      
+      nElectrons += (nAtoms * Z);
+      effectiveZ += (nAtoms * Z * pow(Z, 2.94));
+      if(strcmp(name, "hdpe") == 0) cout << name << ", Z=" << getElement(Z) << ", nAtoms=" << nAtoms << ", mass=" << getMass(Z) << ", nElectrons = " << nAtoms * Z << endl;
     }
+
+    effectiveZ = pow(effectiveZ / nElectrons, 1./2.94);
+    fileOut5 << name << ", " << formula << ", " << effectiveZ << endl;
+    
     fileOut << "materials_[\"" << name <<  "\"] = " << name << ";" << endl;
     fileOut2 << "density.push_back(" << density << ");  material.push_back(\"" << name << "\");" << endl;
     
@@ -206,26 +266,27 @@ void makeElements(ofstream &file){
 int convert(){
 
   vector<pair<string, string> > fileName;
-  //  fileName.push_back(pair<string, string> ("Drugs_Library.txt", "DRUGS"));
-  //  fileName.push_back(pair<string, string> ("Explosives_Library.txt", "EXPLOSIVES"));
-  //  fileName.push_back(pair<string, string> ("Other_library.txt", "OTHERS"));
-  //  fileName.push_back(pair<string, string> ("UXO.txt", "UXO"));
-  //  fileName.push_back(pair<string, string> ("Explosives_Libraryv2.txt", "EXPLOSIVES")); // new from 2020-08-29 after bug fix
-  //  fileName.push_back(pair<string, string> ("Other_libraryv2.txt", "OTHERS")); // new from 2020-08-29 after bug fix
-  //  fileName.push_back(pair<string, string> ("Shielding_Materialsv2.txt", "SHIELDING"));
-  //  fileName.push_back(pair<string, string> ("Shielding_Materialsv3.txt", "SHIELDING"));
-  //  fileName.push_back(pair<string, string> ("Shielding_Materialsv4.txt", "SHIELDING"));
-  //  fileName.push_back(pair<string, string> ("Steels.txt", "STEELS"));
-  //  fileName.push_back(pair<string, string> ("Starfire.txt", "STARFIRE"));
+  fileName.push_back(pair<string, string> ("Drugs_Library.txt", "DRUGS"));
+  fileName.push_back(pair<string, string> ("Explosives_Library.txt", "EXPLOSIVES"));
+  fileName.push_back(pair<string, string> ("Other_library.txt", "OTHERS"));
+  fileName.push_back(pair<string, string> ("UXO.txt", "UXO"));
+  fileName.push_back(pair<string, string> ("Explosives_Libraryv2.txt", "EXPLOSIVES")); // new from 2020-08-29 after bug fix
+  fileName.push_back(pair<string, string> ("Other_libraryv2.txt", "OTHERS")); // new from 2020-08-29 after bug fix
+  fileName.push_back(pair<string, string> ("Shielding_Materialsv2.txt", "SHIELDING"));
+  fileName.push_back(pair<string, string> ("Shielding_Materialsv3.txt", "SHIELDING"));
+  fileName.push_back(pair<string, string> ("Shielding_Materialsv4.txt", "SHIELDING"));
+  fileName.push_back(pair<string, string> ("Steels.txt", "STEELS"));
+  fileName.push_back(pair<string, string> ("Starfire.txt", "STARFIRE"));
   fileName.push_back(pair<string, string> ("Showergel.txt", "OTHERS"));
   
   ofstream fileOut("commands.txt");
   ofstream fileOut2("commands2.txt");
   ofstream fileOut3("../standalone/templates/analysis/loopMaterials");
   ofstream fileOut4("commands3.txt");
+  ofstream fileOut5("effectiveAtomicNumber.csv");
   
   for(unsigned int i=0; i<fileName.size(); i++){
-    parseMaterials(fileOut, fileOut2, fileOut3, fileOut4, fileName[i]);        
+    parseMaterials(fileOut, fileOut2, fileOut3, fileOut4, fileOut5, fileName[i]);        
   }
   
   fileOut.close();
