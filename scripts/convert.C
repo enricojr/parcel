c1aeeb07393f7902ae545646746c10763e148409
enricojr <sc_enricojr@yahoo.it>
2020-11-24 15:22:49 +0100

    added soil

--- scripts/convert.C
+++ scripts/convert.C
@@ -266,18 +266,19 @@ void makeElements(ofstream &file){
 int convert(){
 
   vector<pair<string, string> > fileName;
-  fileName.push_back(pair<string, string> ("Drugs_Library.txt", "DRUGS"));
-  fileName.push_back(pair<string, string> ("Explosives_Library.txt", "EXPLOSIVES"));
-  fileName.push_back(pair<string, string> ("Other_library.txt", "OTHERS"));
-  fileName.push_back(pair<string, string> ("UXO.txt", "UXO"));
-  fileName.push_back(pair<string, string> ("Explosives_Libraryv2.txt", "EXPLOSIVES")); // new from 2020-08-29 after bug fix
-  fileName.push_back(pair<string, string> ("Other_libraryv2.txt", "OTHERS")); // new from 2020-08-29 after bug fix
-  fileName.push_back(pair<string, string> ("Shielding_Materialsv2.txt", "SHIELDING"));
-  fileName.push_back(pair<string, string> ("Shielding_Materialsv3.txt", "SHIELDING"));
-  fileName.push_back(pair<string, string> ("Shielding_Materialsv4.txt", "SHIELDING"));
-  fileName.push_back(pair<string, string> ("Steels.txt", "STEELS"));
-  fileName.push_back(pair<string, string> ("Starfire.txt", "STARFIRE"));
-  fileName.push_back(pair<string, string> ("Showergel.txt", "OTHERS"));
+  // fileName.push_back(pair<string, string> ("Drugs_Library.txt", "DRUGS"));
+  // fileName.push_back(pair<string, string> ("Explosives_Library.txt", "EXPLOSIVES"));
+  // fileName.push_back(pair<string, string> ("Other_library.txt", "OTHERS"));
+  // fileName.push_back(pair<string, string> ("UXO.txt", "UXO"));
+  // fileName.push_back(pair<string, string> ("Explosives_Libraryv2.txt", "EXPLOSIVES")); // new from 2020-08-29 after bug fix
+  // fileName.push_back(pair<string, string> ("Other_libraryv2.txt", "OTHERS")); // new from 2020-08-29 after bug fix
+  // fileName.push_back(pair<string, string> ("Shielding_Materialsv2.txt", "SHIELDING"));
+  // fileName.push_back(pair<string, string> ("Shielding_Materialsv3.txt", "SHIELDING"));
+  // fileName.push_back(pair<string, string> ("Shielding_Materialsv4.txt", "SHIELDING"));
+  // fileName.push_back(pair<string, string> ("Steels.txt", "STEELS"));
+  // fileName.push_back(pair<string, string> ("Starfire.txt", "STARFIRE"));
+  //  fileName.push_back(pair<string, string> ("Showergel.txt", "OTHERS"));
+  fileName.push_back(pair<string, string> ("BasaltSoil.txt", "SOIL"));
   
   ofstream fileOut("commands.txt");
   ofstream fileOut2("commands2.txt");
